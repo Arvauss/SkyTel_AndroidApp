fb6f825af60323206e3f18f99d44b365a74e3db0
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-11-01 20:07:01 +0200

    Attempted to make links and Image Buttons clickable

--- app/src/main/java/com/example/skytel_mobileapp/ContactUs.java
+++ app/src/main/java/com/example/skytel_mobileapp/ContactUs.java
@@ -3,6 +3,9 @@ package com.example.skytel_mobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        //LINK FOR LEGAL
+        ((TextView) findViewById(R.id.legalLink)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.legalLink)).setText(Html.fromHtml(getResources().getString(R.string.LEGAL)));
+
+        //LINK FOR PRIVACY POLICY
+        ((TextView) findViewById(R.id.privacypolicylink)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.privacypolicylink)).setText(Html.fromHtml(getResources().getString(R.string.PRIVACY_POLICY)));
+
+        //LINK FOR T'S AND C'S
+        ((TextView) findViewById(R.id.tcandcslink)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.tcandcslink)).setText(Html.fromHtml(getResources().getString(R.string.TC)));
+
     }
 }
