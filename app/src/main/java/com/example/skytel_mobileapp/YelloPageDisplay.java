5cb7f57958dd9505c3c0561d331a182c4cb6a2be
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-11-01 13:25:32 +0200

    Yello Page Interface Done - Consider using intents for backend (Stack overflow)

---
+++ app/src/main/java/com/example/skytel_mobileapp/YelloPageDisplay.java
@@ -0,0 +1,14 @@
+package com.example.skytel_mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YelloPageDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yello_page_display);
+    }
+}
